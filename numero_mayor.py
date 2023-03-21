2b5f92ed2b7033b2bc426a57389b61f466220fbb
SheilynG <sheilyndelacruzsanchez@gmail.com>
2023-03-21 09:16:17 -0600

    cree un pequeño programa en Python que le pida al usuario 2 números y el programa le indica cual es el más grande

---
+++ numero_mayor.py
@@ -0,0 +1,8 @@
+print('¿Cuál es el número mayor')
+n1  = input('Ingrese un valor: ')
+n2 = input('Ingrese un otro valor: ')
+
+if n1> n2:
+    print(f'{n1} es el mayor')
+elif n2> n1:
+    print(f'{n2} es el mayor')
